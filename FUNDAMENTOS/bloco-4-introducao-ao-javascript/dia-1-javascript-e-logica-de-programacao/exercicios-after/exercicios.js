e62d9e8e6134c2a8aed19ee06589121e31b5350a
Felipe Seabra <feliperoce@gmail.com>
2022-06-21 18:31:34 -0300

    Resolução do exercicio 1

---
+++ FUNDAMENTOS/bloco-4-introducao-ao-javascript/dia-1-javascript-e-logica-de-programacao/exercicios-after/exercicios.js
@@ -0,0 +1,29 @@
+let a = 10;
+let b = 20;
+let resultado;
+
+// ex 1:
+// adição
+
+resultado = a + b;
+console.log(resultado);
+
+//subtração
+
+resultado = a - b;
+console.log(resultado);
+
+//multiplicação
+
+resultado = a * b;
+console.log(resultado);
+
+//divisão
+
+resultado = a / b;
+console.log(resultado);
+
+//módulo (resto)
+
+resultado = a % b;
+console.log(resultado);
