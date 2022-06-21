38c4a7bccec87bba23a147d761469629d7e3a328
Felipe Seabra <feliperoce@gmail.com>
2022-06-21 18:54:38 -0300

    Resolução do exercicio 5

--- FUNDAMENTOS/bloco-4-introducao-ao-javascript/dia-1-javascript-e-logica-de-programacao/exercicios-after/exercicios.js
+++ FUNDAMENTOS/bloco-4-introducao-ao-javascript/dia-1-javascript-e-logica-de-programacao/exercicios-after/exercicios.js
@@ -60,3 +60,17 @@ if (valor > 0) {
 }else {
     console.log("zero");
 }
+
+// ex 5:
+const ang1 = 60;
+const ang2 = 70;
+const ang3 = 50;
+
+if ( ang1 + ang2 + ang3 === 180)
+{
+    console.log(true);
+}else if (ang1 + ang2 > 0 && ang2 + ang3 > 0) {
+    console.log(false);
+}else {
+    console.log("Erro");
+}
