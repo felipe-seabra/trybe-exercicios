08e431d5e19d1090529865c3022532073dd991d2
Felipe Seabra <feliperoce@gmail.com>
2022-06-21 15:32:58 -0300

    Realização do primeiro exercicio

---
+++ FUNDAMENTOS/bloco-4-introducao-ao-javascript/b4-d1/exercicio-1.js
@@ -0,0 +1,15 @@
+const myName = "Felipe";
+const birthCity = "Presidente Prudente";
+let birthYear = 1990;
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
+
+birthYear = 2030;
+
+console.log(birthYear);
+
+birthCity = "Presidente Venceslau"
+
+// O erro ocorre pois não é possível alterar uma constante.
