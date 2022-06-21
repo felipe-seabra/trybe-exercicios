f93f3fcd8a924848fae6433ea41517d502942942
Felipe Seabra <feliperoce@gmail.com>
2022-06-21 18:44:05 -0300

    Resolução do exercicio 3

--- FUNDAMENTOS/bloco-4-introducao-ao-javascript/dia-1-javascript-e-logica-de-programacao/exercicios-after/exercicios.js
+++ FUNDAMENTOS/bloco-4-introducao-ao-javascript/dia-1-javascript-e-logica-de-programacao/exercicios-after/exercicios.js
@@ -34,3 +34,18 @@ if (a > b) {
 }else {
     console.log("B é maior que A");
 }
+
+//ex 3:
+const num1 = 10;
+const num2 = 20;
+const num3 = 30;
+
+if (num1 > num2 && num1 > num3) {
+    console.log("O primeiro número é maior")
+}else if (num2 > num1 && num2 > num3) {
+    console.log("O segundo número é maior")
+}else if (num3 > num1 && num3 > num2) {
+    console.log("O terceiro número é maior")
+}else {
+    console.log("Tem algo errado");
+}
