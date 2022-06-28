f8c91503b14d470ffffd19eb9bb96ff0c2a4e3d9
Felipe Seabra <feliperoce@gmail.com>
2022-06-28 16:50:56 -0300

    Resolvendo exercicio de Administrado do Tempo

--- FUNDAMENTOS/bloco-5-dom/praticando/script.js
+++ FUNDAMENTOS/bloco-5-dom/praticando/script.js
@@ -1,17 +1,24 @@
 let headerContainer = document.getElementById("header-container");
 headerContainer.style.backgroundColor = "green";
 
-let Tasks = document.querySelectorAll("body h3");
-Tasks[0].style.backgroundColor = "purple";
-Tasks[1].style.backgroundColor = "purple";
-Tasks[2].style.backgroundColor = "black";
-Tasks[3].style.backgroundColor = "black";
-
 let bgTask = document.getElementsByClassName("emergency-tasks");
 bgTask[0].style.backgroundColor = "LightCoral";
 
 let bgTaskRight = document.getElementsByClassName("no-emergency-tasks");
-bgTaskRight[0].style.backgroundColor = "Gold"
+bgTaskRight[0].style.backgroundColor = "Gold";
 
 let footer = document.getElementById("footer-container");
 footer.style.backgroundColor = "DarkSlateGray";
+
+function colorTask(colorOne, colorTwo, verif, task) {
+  for (let i in task) {
+    if (i < verif / 2) {
+      tasks[i].style.backgroundColor = colorOne;
+    } else {
+      tasks[i].style.backgroundColor = colorTwo;
+    }
+  }
+}
+
+let tasks = document.querySelectorAll("body h3");
+colorTask("purple", "black", tasks.length, tasks);
