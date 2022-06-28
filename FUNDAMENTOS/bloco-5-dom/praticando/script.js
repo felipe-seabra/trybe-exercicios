0ef459f8e3f35edbc14f63d89cb97ebc12986685
Felipe Seabra <feliperoce@gmail.com>
2022-06-28 16:01:10 -0300

    Criando administrador de tempo e estilizando com js

---
+++ FUNDAMENTOS/bloco-5-dom/praticando/script.js
@@ -0,0 +1,17 @@
+let headerContainer = document.getElementById("header-container");
+headerContainer.style.backgroundColor = "green";
+
+let Tasks = document.querySelectorAll("body h3");
+Tasks[0].style.backgroundColor = "purple";
+Tasks[1].style.backgroundColor = "purple";
+Tasks[2].style.backgroundColor = "black";
+Tasks[3].style.backgroundColor = "black";
+
+let bgTask = document.getElementsByClassName("emergency-tasks");
+bgTask[0].style.backgroundColor = "LightCoral";
+
+let bgTaskRight = document.getElementsByClassName("no-emergency-tasks");
+bgTaskRight[0].style.backgroundColor = "Gold"
+
+let footer = document.getElementById("footer-container");
+footer.style.backgroundColor = "DarkSlateGray";
