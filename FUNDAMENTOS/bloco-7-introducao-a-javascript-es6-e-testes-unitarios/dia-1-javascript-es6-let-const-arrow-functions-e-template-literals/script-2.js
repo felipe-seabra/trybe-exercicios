e7b951dabf38f33f2d6095a269c3e12605354842
Felipe Seabra <feliperoce@gmail.com>
2022-07-14 18:36:14 -0300

    Resolvendo exercicio 2

---
+++ FUNDAMENTOS/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/script-2.js
@@ -0,0 +1,7 @@
+// 🚀 Crie uma função que retorne um array em ordem crescente.
+
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+const sortNumbers = (oddsAndEvens) => `Os números ${oddsAndEvens.sort((a, b) => a - b)} se encontram ordenados de forma crescente!`;
+
+console.log(sortNumbers(oddsAndEvens)); // será necessário alterar essa linha 😉
